ebb18235acc941ebb8e4d3783ffac04c98bd9044
Dan Meliza <dan@meliza.org>
2019-07-06 14:27:55 -0400

    fix timezone formatting for linux

--- appdir/track_oddball/app.R
+++ appdir/track_oddball/app.R
@@ -9,7 +9,7 @@ library(gtools)
 
 ui <- fluidPage(
   titlePanel("Operant DataViz"),
-  
+
   sidebarLayout(
     sidebarPanel(
       uiOutput('subj'),
@@ -38,14 +38,14 @@ ui <- fluidPage(
 )
 
 server <- function(input,output) {
-  
+
   #Get list of subjects from API
   output$subj = renderUI({
     s = fromJSON(url("https://aplonis.psyc.virginia.edu/decide/api/subjects/"))
     subjlist = s$name[s$name!="dummy"]
     selectInput('subject','Subject:',subjlist)
   })
-  
+
   #Set start date of data request
   startdate = reactive({
     if (input$time == 1) {
@@ -62,7 +62,7 @@ server <- function(input,output) {
       as.POSIXct(t1)
     }
   })
-  
+
   #Set end date of data request
   stopdate = reactive({
     if (input$time == 2) {
@@ -75,7 +75,7 @@ server <- function(input,output) {
       as.POSIXct(t2)
     }
   })
-  
+
   #Set plot label based on time selection
   labeltype = reactive({
     if (input$time == 1 | input$time == 2) {
@@ -84,7 +84,7 @@ server <- function(input,output) {
       "%D"
     }
   })
-  
+
   #Enable data update with button press
   update = eventReactive(input$update, {}, ignoreNULL = FALSE)
 
@@ -93,7 +93,7 @@ server <- function(input,output) {
     req(input$subject)
     u = update()
     subj = paste("https://aplonis.psyc.virginia.edu/decide/api/trials/?nocomment=True&no_page&subject=", input$subject, sep="")
-    timerange = paste(strftime(startdate(), "%Y-%m-%dT%H:%M:%S%z"),strftime(stopdate(), "%Y-%m-%dT%H:%M:%S%z"), sep=',')
+    timerange = paste(strftime(startdate(), "%Y-%m-%dT%H:%M:%S"),strftime(stopdate(), "%Y-%m-%dT%H:%M:%S"), sep=',')
     query = paste(subj, paste('time__range', timerange, sep = "="), sep = "&")
     #print(query)
     resp = GET(url = query)
@@ -122,18 +122,18 @@ server <- function(input,output) {
     trials$outcome = with(trials, interaction(response,correct))
     trials
   })
-  
+
   #Get list of experiments within time range
   output$exp = renderUI({
     selectInput('experiment','Experiment:',unique(timedata()$experiment[!is.na(timedata()$experiment)]))
   })
-  
+
   #Filter data for chosen experiment
   expdata = reactive({
     req(input$experiment)
     filter(timedata(),experiment == input$experiment)
   })
-  
+
   #Show number of records returned by API
   output$records = renderText({
     req(input$experiment)
@@ -143,7 +143,7 @@ server <- function(input,output) {
       "Fetching records..."
     }
   })
-  
+
   #Plotting code
   output$cummulative = renderPlot({
     req(input$experiment)
@@ -151,54 +151,54 @@ server <- function(input,output) {
       return()
     } else {
       ggplot(expdata(),aes(x=date,y=cumul)) +
-        geom_line() + 
-        geom_point() + 
-        scale_x_datetime(date_labels=labeltype(),limits=c(min(expdata()$date),max(expdata()$date))) + 
-        xlab("Time of trials") + 
-        ylab("Cumulative correct trials") + 
+        geom_line() +
+        geom_point() +
+        scale_x_datetime(date_labels=labeltype(),limits=c(min(expdata()$date),max(expdata()$date))) +
+        xlab("Time of trials") +
+        ylab("Cumulative correct trials") +
         ggtitle("Correct trials over time")
     }
   })
-  
+
   bw = reactive({ 2 * IQR(expdata()$date) / length(expdata()$date)^(1/3) })
-  
+
   output$outcome = renderPlot({
-    ggplot(expdata(), aes(date,fill=outcome)) + 
-      geom_histogram(binwidth = bw()) + 
-      xlab("Days") + 
-      ylab("Number of trials") + 
+    ggplot(expdata(), aes(date,fill=outcome)) +
+      geom_histogram(binwidth = bw()) +
+      xlab("Days") +
+      ylab("Number of trials") +
       ggtitle("Summary of performance over time")
   })
-  
+
   output$rt = renderPlot({
     cortr = filter(expdata(), outcome=="peck_left.TRUE")
-    ggplot(cortr, aes(x=ind,y=rtime)) + 
-      geom_point() + 
-      geom_smooth(method='lm', se = TRUE) + 
-      xlab("Trials") + 
-      ylab("Response time") + 
+    ggplot(cortr, aes(x=ind,y=rtime)) +
+      geom_point() +
+      geom_smooth(method='lm', se = TRUE) +
+      xlab("Trials") +
+      ylab("Response time") +
       ggtitle("Response time of pecks after target stimulus")
   })
-  
+
   output$cumindex = renderPlot({
-    ggplot(expdata(),aes(x=ind,y=cumul)) + 
-      geom_line() + 
+    ggplot(expdata(),aes(x=ind,y=cumul)) +
+      geom_line() +
       geom_point() +
-      xlab("Trials") + 
+      xlab("Trials") +
       ylab("Correct responses") +
       ggtitle("Cummulative correct responses across trials")
   })
-  
+
   output$mistake = renderPlot({
     early = filter(expdata(), outcome=="stimA.FALSE")
-    ggplot(early, aes(x=ind,y=rtime)) + 
-      geom_point() + 
+    ggplot(early, aes(x=ind,y=rtime)) +
+      geom_point() +
       geom_smooth(method='lm', se = TRUE) +
       xlab("Trials") +
       ylab("Response time") +
       ggtitle("Response time for incorrect pecks")
   })
-  
+
   #Enable download of plotting data
   output$downloadData <- downloadHandler(
     filename = function() {
